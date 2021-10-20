cc382f965bed4dc94ae703ef03689b99b5e02218
Vesa Selkälä <vesa.selkala@gmail.com>
2021-10-20 15:10:06 +0300

    Olion luonti

---
+++ project.js
@@ -0,0 +1,18 @@
+const sitaatit = {
+    _politiikka,
+    _urheilu,
+    _kirjallisuus,
+    _sananlaskut,
+    get politiikka(){
+        return this._politiikka
+    },
+    get urheilu(){
+        return this._urheilu
+    },
+    get kirjallisuus(){
+        return this._kirjallisuus
+    },
+    get sananlaskut(){
+        return this._sananlaskut
+    },
+}
