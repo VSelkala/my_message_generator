a02d8d1236eed174275c583d0e05d282225a35bd
Vesa Selkälä <vesa.selkala@gmail.com>
2021-10-20 15:36:24 +0300

    Added getters and setters

--- project.js
+++ project.js
@@ -1,18 +1,47 @@
 const sitaatit = {
-    _politiikka,
-    _urheilu,
-    _kirjallisuus,
-    _sananlaskut,
+    _politiikka: [],
+    _urheilu: [],
+    _kirjallisuus: [],
+    _sananlaskut: [],
     get politiikka(){
         return this._politiikka
+    },
+    set politiikka(politiikkaIn){
+        if (typeof politiikkaIn === 'string'){
+            this._politiikka.push(politiikkaIn)
+        } else {
+            console.log('Virhe: syötettävän tiedon tulee olla tekstimuodossa')
+        }
     },
     get urheilu(){
         return this._urheilu
     },
+    set urheilu(urheiluIn){
+        if (typeof urheiluIn === 'string'){
+            this._urheilu.push(urheiluIn)
+        } else {
+            console.log('Virhe: syötettävän tiedon tulee olla tekstimuodossa')
+        }
+    },
     get kirjallisuus(){
         return this._kirjallisuus
+    },
+    set kirjallisuus(kirjallisuusIn){
+        if (typeof kirjallisuusIn === 'string'){
+            this._kirjallisuus.push(kirjallisuusIn)
+        } else {
+            console.log('Virhe: syötettävän tiedon tulee olla tekstimuodossa')
+        }
     },
     get sananlaskut(){
         return this._sananlaskut
     },
-}
+    set sananlaskut(sananlaskutIn){
+        if (typeof sananlaskutIn === 'string'){
+            this._sananlaskut.push(sananlaskutIn)
+        } else {
+            console.log('Virhe: syötettävän tiedon tulee olla tekstimuodossa')
+        }
+    },
+};
+
