e6d46324f14609d336993691f9d9097ad6a24efe
Vesa Selkälä <vesa.selkala@gmail.com>
2021-10-21 12:41:00 +0300

    Added names and a bit of functionality

--- project.js
+++ project.js
@@ -1,35 +1,58 @@
-const name = {
+const rapperName = {
     _firstName: [],
     _middleName: [],
     _lastName: [],
     get firstName(){
-        return this._firstName
+        return this._firstName[Math.floor(Math.random * this._firstName.length)]
     },
     set firstName(firstNameIn){
         if (typeof firstNameIn === 'string'){
             this._firstName.push(firstNameIn)
         } else {
-            console.log('Virhe: syötettävän tiedon tulee olla tekstimuodossa')
+            console.log('ERROR: String needed')
         }
     },
     get middleName(){
-        return this._upperBody
+        return this._middleName[Math.floor(Math.random * this._middleName.length)]
     },
     set middleName(middleNameIn){
         if (typeof middleNameIn === 'string'){
             this._middleName.push(middleNameIn)
         } else {
-            console.log('Virhe: syötettävän tiedon tulee olla tekstimuodossa')
+            console.log('ERROR: String needed')
         }
     },
     get lastName(){
-        return this._lastName
+        return this._lastName[Math.floor(Math.random * this._lastName.length)]
     },
     set lastName(lastNameIn){
         if (typeof lastNameIn === 'string'){
             this._lastName.push(lastNameIn)
         } else {
-            console.log('Virhe: syötettävän tiedon tulee olla tekstimuodossa')
+            console.log('ERROR: String needed')
         }
     },
 };
+
+rapperName.firstName('Icy')
+rapperName.firstName('Dr.')
+rapperName.firstName('Slim')
+rapperName.firstName('Dirty')
+rapperName.firstName('Old')
+rapperName.firstName('The')
+rapperName.firstName('Big')
+rapperName.firstName('Lil')
+rapperName.middleName('Ice')
+rapperName.middleName('Hard')
+rapperName.middleName('L.')
+rapperName.middleName('')
+rapperName.middleName('Dirty')
+rapperName.middleName('Shady')
+rapperName.lastName('Bastard')
+rapperName.lastName('Cube')
+rapperName.lastName('Tea')
+rapperName.lastName('Killa')
+rapperName.lastName('Rhymes')
+rapperName.lastName('Smalls')
+rapperName.lastName('X')
+
